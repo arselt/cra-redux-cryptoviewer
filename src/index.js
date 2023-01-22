7533b671635b8c695422150be90f756bd15a209f
Arath Astorga <arastorga@outlook.com>
2023-01-21 21:36:03 -0600

    chore: add middleware logger and devtools

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { cryptosReducer } from "./reducers/cryptos";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
+import { logger } from "./middlewares";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = createStore(cryptosReducer);
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), //add redux devtools
+  applyMiddleware(logger)
+);
+
+const store = createStore(
+  cryptosReducer,
+  composedEnhancers
+);
 
 root.render(
   <Provider store={store}>
