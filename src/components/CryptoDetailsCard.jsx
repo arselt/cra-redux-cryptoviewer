1e92321d18936e0868f6c045ef751a77c2f81d7d
Arath Astorga <arastorga@outlook.com>
2023-01-28 18:44:15 -0600

    fix: add loading state to CryptoDetailsCard

--- src/components/CryptoDetailsCard.jsx
+++ src/components/CryptoDetailsCard.jsx
@@ -1,16 +1,19 @@
 import { getCryptoDetails } from "../api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCryptoDetails } from "../actions";
 
 const CryptoDetailsCard = ({cryptoId}) => {
   const cryptoDetails = useSelector(state => state.cryptoDetails)
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchCryptos = async () => {
+      setLoading(true);
       const cryptosDetailRes = await getCryptoDetails(cryptoId);
       dispatch(setCryptoDetails(cryptosDetailRes));
+      setLoading(false);
     };
     fetchCryptos();
   }, [])
@@ -18,11 +21,10 @@ const CryptoDetailsCard = ({cryptoId}) => {
   return (
     <div className="w-full flex justify-center content-center items-center h-32">
       <p className="px-4">
-        {cryptoDetails.last_updated}
+        {loading ? 'Loading...' : cryptoDetails.last_updated}
       </p>
     </div>
   )
 }
-
 
 export default CryptoDetailsCard;
